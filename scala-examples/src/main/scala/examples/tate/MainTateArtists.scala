718b0f1b83864849ab35359fb2041a10426d0ff7
seralf <seralf@gmail.com>
2018-12-21 00:54:18 +0100

    added naive using example on stream

---
+++ scala-examples/src/main/scala/examples/tate/MainTateArtists.scala
@@ -0,0 +1,36 @@
+package examples.tate
+
+import scala.io.Source
+import scala.concurrent.Future
+
+import scala.concurrent.ExecutionContext.Implicits.global._
+
+object MainTateArtists extends App {
+
+  val artists_url = "https://raw.githubusercontent.com/tategallery/collection/master/artist_data.csv"
+
+  val src = Source.fromURL(artists_url)("UTF-8")
+
+  // SEE
+  //  val content: Stream[String] = src.getLines().toStream
+  //  content
+  //    .zipWithIndex
+  //    .foreach {
+  //      case (line, i) =>
+  //        println(i + ":" + line)
+  //    }
+  //  src.close()
+
+  using(src) { input =>
+    input.getLines().toStream
+  }(_.close())
+    .foreach(println)
+
+  // naive example
+  def using[T <: { def close() }, R](resource: T)(action: T => R)(rsc: T => Unit): R = {
+
+    action(resource)
+
+  }
+
+}
